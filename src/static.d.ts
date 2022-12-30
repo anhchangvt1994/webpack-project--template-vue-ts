bfb7e22a86f7941f2070f8294f50a4fe65c9161b
Truong <anhchangvt1994@gmail.com>
2022-12-31 01:18:00 +0700

    update(other): 🧩 autocomplete static extension
    can autocomplete image extension when use import and require + declare
    
    BREAKING CHANGE: 🧨 - autocomplete image extension
    
    ✅ Closes: q!

---
+++ src/static.d.ts
@@ -0,0 +1,48 @@
+declare module '*.svg' {
+	const value: any
+	export = value
+}
+declare module '*.png' {
+	const value: any
+	export = value
+}
+declare module '*.jpg' {
+	const value: any
+	export = value
+}
+declare module '*.jpeg' {
+	const value: any
+	export = value
+}
+declare module '*.webp' {
+	const value: any
+	export = value
+}
+declare module '*.mp3' {
+	const value: any
+	export = value
+}
+declare module '*.mp4' {
+	const value: any
+	export = value
+}
+declare module '*.mov' {
+	const value: any
+	export = value
+}
+declare module '*.mkv' {
+	const value: any
+	export = value
+}
+declare module '*.webm' {
+	const value: any
+	export = value
+}
+
+
+can autocomplete image extension when use import and require + declare
+module for it.
+- autocomplete image extension
+- hidden autocomplete module
+- set globalThis _VUE_OPTIONS_API_
+- set globalThis _VUE_PROD_DEVTOOLS_
